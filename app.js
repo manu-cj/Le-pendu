f71c81dfeb57d5589830f6c73bf7cdab875a8e73
manucarenjot <carenjotje@gmail.com>
2021-12-03 13:57:24 +0100

    Le pendu

---
+++ app.js
@@ -0,0 +1,12 @@
+let error = document.getElementById('erreur');
+let good = document.getElementById('bonne-réponses');
+let input = document.getElementById('input');
+let button = document.getElementById('send');
+let chances = document.getElementById('chances');
+
+chance = 7;
+
+let mots = ['kebab', 'lotus', 'famille', 'lubullule', 'eolienne', 'chien', 'katana',
+    'reveil', 'crayon', 'bug', 'zemour', 'genetique', 'negatif',];
+
+
